c9e6af2657b20b7d6d56d2fefeeabfc1852e8f89
Gamelmohamed <138955241+Gamelmohamed@users.noreply.github.com>
2024-03-28 13:44:21 +0200

    mod calc

--- script.js
+++ script.js
@@ -1,27 +1,78 @@
-const inPutElement = document.querySelectorAll(".card__input")
-const submitButton = document.querySelector(".card__button")
+const inPutElements = document.querySelectorAll (".card__input");
+const submitButton = document.querySelector(".card__button");
 
-const calculateAge = (year, month, day) => { 
-    const today = new Date();
-    const birthDate = new Date(year, month-1, day);
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const monthDff = today.getMonth() - birthDate.getMonth();
-    if (monthDff<0 || (monthDff === 0 && today.getDate()<birthDate.getDate())){
-        age--
-    }
-    return age;
+const validateDay = (day) => {
+if (day && day > 0 && day <= 31){
+return true
+}
+}
+
+const validateMonth = (month) => {
+if (month && month > 0 && month <= 31){
+return true
+}
+}
+
+const validateYear = (year) => {
+if (year && year > 0 && year <= 31){
+return true
+}
 }
 
 
-const onClickHandler = () =>{
-const dayElement = document.querySelector(".card__input[name=day]")
-const monthElement = document.querySelector(".card__input[name=month]")
-const yearElement = document.querySelector(".card__input[name=year]")
-const resultElement = document.querySelector(".card__resultValue")
+const calculateAgeMonth = (year, month, day) => {
+  const age = {
+    year : null,
+    month : null,
+    day : null
+  }
+  const today = new Date();
+  const birthDate = new Date(year, month - 1, day);
+  let ageMonth = today.getMonth() - birthDate.getMonth();
+  let ageYear = today.getFullYear() - birthDate.getFullYear();
+  var monthDff = today.getMonth() - birthDate.getMonth();
+  var ageDay = today.getDate() - birthDate.getDate();
+  
+  if (
+    monthDff < 0 
+  ) {
+    ageYear --
+    ageMonth +=12;
+  }
+  if(ageDay < 0){
+    ageMonth--
+    ageDay +=30
+  } 
 
-resultElement.textContent = calculateAge(yearElement.value, monthElement.value, dayElement.value)
-    console.log(resultElement)
-}
+age.year = ageYear
+age.month = ageMonth
+age.day = ageDay
+  return age;
+};
 
-submitButton.addEventListener("click", onClickHandler)
+const onClickHandler = () => {
+  const dayElement = document.querySelector(".card__input[name=day]");
+  const monthElement = document.querySelector(".card__input[name=month]");
+  const yearElement = document.querySelector(".card__input[name=year]");
+  const resultYear = document.querySelector(".card__resultValue[name=year]");
+  const resultMonth = document.querySelector(".card__resultValue[name=month]");
+  const resultDay = document.querySelector(".card__resultValue[name=day]");
 
+  let age = calculateAgeMonth(
+    yearElement.value,
+    monthElement.value,
+    dayElement.value
+  );
+  resultYear.textContent = age.year
+  resultMonth.textContent = age.month
+  resultDay.textContent = age.day
+  
+};
+
+inPutElements.forEach((item) => {
+  item.addEventListener("keydown", (event) => {
+    event.key === "Enter" && onClickHandler();
+  });
+});
+
+submitButton.addEventListener("click", onClickHandler);
